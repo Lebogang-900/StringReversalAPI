72f7a92b8b5bc9cb2e2cf2b3d65f4be4b2559092
LebogangC <LebogangC@LEBOGANG-ACER>
2024-07-22 11:51:51 +0200

    Add project files.

---
+++ Controllers/StringReversalController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StringReversalAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StringReversalController : Controller
+    {
+        StringHelper stringHelper;
+
+        public StringReversalController()
+        {
+            stringHelper = new StringHelper();
+        }
+
+        [HttpGet("reverseStringAndCheckPalindrome")]
+        public IActionResult ReverseStringAndCheckPalindrome(string inputString) 
+        {
+            //first check if string is not empty or it is not a null value.
+            if (string.IsNullOrEmpty(inputString)) 
+            {
+                return BadRequest("Input string must not be null or empty.");
+            }
+
+            //reverse the string and check if 
+            var reversedString = stringHelper.ReverseString(inputString);
+            var isPalindrome = stringHelper.IsPalindrome(inputString);
+
+            var result = new
+            {
+                ReversedString = reversedString,
+                IsPalindrome = isPalindrome
+            };
+
+            return Ok(result);
+        }
+    }
+}
---
+++ StringHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace StringReversalAPI
+{
+    public class StringHelper
+    {
+        public string ReverseString(string input)
+        {
+            // Convert the input string to a character array.
+            char[] charArray = input.ToCharArray();
+
+            // Reverse the character array in place.
+            Array.Reverse(charArray);
+
+            // Create a new string from the reversed character array.
+            return new string(charArray);
+        }
+
+        public bool IsPalindrome(string input)
+        {
+            var reversedString = ReverseString(input);
+
+            // Compare the original string with the reversed string
+            return string.Equals(input, reversedString, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+
+
+}
